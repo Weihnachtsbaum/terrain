c522b790b1884634ab9397ac08cba09661a157fc
Weihnachtsbaum <140716472+Weihnachtsbaum@users.noreply.github.com>
2025-08-16 20:05:13 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
+#![cfg_attr(not(feature = "console"), windows_subsystem = "windows")]
+
+use bevy::prelude::*;
+
+fn main() -> AppExit {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run()
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera3d::default());
+}
